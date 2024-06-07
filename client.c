d8858266d69a3597b1b5e144415f4b3cfbd4c31e
Sanda Hajaina Ranaivoarisoa <sranaivo@c1r7s5.42antananarivo.mg>
2024-06-07 16:54:18 +0300

    new code

--- client.c
+++ client.c
@@ -10,40 +10,103 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void    send_signal(int server_pid, char *message)
+volatile sig_atomic_t	ack_received = 0;
+volatile sig_atomic_t	msg_received = 0;
+
+void	ack_handler(int signal)
 {
-    int i;
-    int bit;
-
-    i = -1;
-    while (message[++i])
-    {
-        bit = 7;
-        while (bit >= 0)
-        {
-            if (message[i] & (1 << bit))
-                kill(server_pid, SIGUSR1);
-            else
-                kill(server_pid, SIGUSR2);
-            usleep(50);
-            bit--;
-        }
-    }
+	ack_received = 1;
 }
 
-int main(int argc,char *argv[])
+void	msg_handler(int signal)
 {
-    pid_t   server_pid;
-    char    *message;
+	msg_received = 1;
+}
 
-    server_pid = (pid_t)atoi(argv[1]);
-    message = argv[2];
-    printf("Client PID: %d\n", getpid());
+void	send_signal(int server_pid, char *message)
+{
+	int	i;
+	int	bit;
 
-    send_signal(server_pid, message);
-    
-    return (0);
+	i = 0;
+	while (message[i] != '\0')
+	{
+		bit = 7;
+		while (bit >= 0)
+		{
+			ack_received = 0;
+			if (message[i] & (1 << bit))
+			{
+				kill(server_pid, SIGUSR1);
+			}
+			else
+			{
+				kill(server_pid, SIGUSR2);
+			}
+			while (!ack_received)
+			{
+				usleep(100); // Wait for acknowledgment
+			}
+			bit--;
+		}
+		i++;
+	}
+	// Send null character to signal end of message
+	bit = 7;
+	while (bit >= 0)
+	{
+		ack_received = 0;
+		kill(server_pid, SIGUSR2);
+		while (!ack_received)
+		{
+			usleep(100); // Wait for acknowledgment
+		}
+		bit--;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	pid_t				server_pid;
+	char				*message;
+	struct sigaction	ack_action;
+	struct sigaction	msg_action;
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <server_pid> <message>\n", argv[0]);
+		return (1);
+	}
+	server_pid = (pid_t)atoi(argv[1]);
+	message = argv[2];
+	ack_action.sa_handler = &ack_handler;
+	sigemptyset(&ack_action.sa_mask);
+	ack_action.sa_flags = 0;
+	if (sigaction(SIGUSR1, &ack_action, NULL) == -1)
+	{
+		perror("sigaction");
+		return (1);
+	}
+	msg_action.sa_handler = &msg_handler;
+	sigemptyset(&msg_action.sa_mask);
+	msg_action.sa_flags = 0;
+	if (sigaction(SIGUSR2, &msg_action, NULL) == -1)
+	{
+		perror("sigaction");
+		return (1);
+	}
+	send_signal(server_pid, message);
+	// Wait for the message received acknowledgment from the server
+	while (!msg_received)
+	{
+		pause();
+	}
+	printf("Message reçu\n");
+	return (0);
 }
--- server.c
+++ server.c
@@ -10,67 +10,133 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-void    s_sig_handler(int signal, siginfo_t *info, void *x)
+typedef struct s_list
 {
-    t_list                  *str;
-    static unsigned char    c = 0;
-    static int              bit = 7;
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
-    if (signal == SIGUSR1)
-    {
-        c |= (1 << bit);
-    }
-    printf("client: %d", info->si_pid);
-    bit--;
-    if (bit < 0)
-    {
-        write(1, &c, 1);
-        c = 0;
-        bit = 7;
-    }
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
 
-void sig_handler(int signal, siginfo_t *info, void *context)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    t_list                  *str;
-    static unsigned char    c = 0;
-    static int              bit = 7;
+	t_list	*temp;
 
-    if (signal == SIGUSR1) {
-        c |= (1 << bit);
-    }
-    bit--;
-    if (bit < 0)
-    {
-        write(1, &c, 1);
-        c = 0;
-        bit = 7;
-    }
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+	}
+	else
+	{
+		temp = *lst;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
 }
 
-int main(void)
+void	ft_lstclear(t_list **lst)
 {
-    struct sigaction    action;
+	t_list	*temp;
 
-    printf("Server PID: %d\n", getpid());
-    action.sa_sigaction = &sig_handler;
-    sigemptyset(&action.sa_mask);
-    action.sa_flags = SA_SIGINFO;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		free((*lst)->content); // Free the content
+		free(*lst);
+		*lst = temp;
+	}
+}
 
-    // if (sigaction(SIGUSR1, &action, NULL) == -1) {
-    //     return 1;
-    // }
-    // if (sigaction(SIGUSR2, &action, NULL) == -1) {
-    //     return 1;
-    // }
-    sigaction(SIGUSR1, &action, NULL);
-    sigaction(SIGUSR2, &action, NULL);
+void	print_list(t_list *lst)
+{
+	t_list	*temp;
 
-    while(1)
-        pause();
-    return (0);
+	temp = lst;
+	while (temp)
+	{
+		printf("%c", *(char *)(temp->content));
+		temp = temp->next;
+	}
+}
+
+void	sig_handler(int signal, siginfo_t *info, void *context)
+{
+	static t_list			*str = NULL;
+	static unsigned char	c = 0;
+	static int				bit = 7;
+	char					*char_ptr;
+	t_list					*new_node;
+
+	if (signal == SIGUSR1)
+	{
+		c |= (1 << bit);
+	}
+	else if (signal == SIGUSR2)
+	{
+		c &= ~(1 << bit);
+	}
+	bit--;
+	if (bit < 0)
+	{
+		char_ptr = malloc(sizeof(char));
+		*char_ptr = c;
+		new_node = ft_lstnew(char_ptr);
+		ft_lstadd_back(&str, new_node);
+		if (c == '\0')
+		{
+			print_list(str);
+			printf("\n");
+			ft_lstclear(&str);
+			// Send signal to client to indicate the message was received
+			kill(info->si_pid, SIGUSR2);
+		}
+		c = 0;
+		bit = 7;
+	}
+	// Send acknowledgment to client
+	kill(info->si_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	struct sigaction	action;
+
+	printf("Server PID: %d\n", getpid());
+	action.sa_sigaction = &sig_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &action, NULL) == -1)
+	{
+		perror("sigaction");
+		return (1);
+	}
+	if (sigaction(SIGUSR2, &action, NULL) == -1)
+	{
+		perror("sigaction");
+		return (1);
+	}
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
